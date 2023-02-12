26ec3f3210b9f6d1cd2ce970ac9054eb2db89b92
lllchak <mediumchak@yandex.ru>
2023-02-12 15:59:19 +0300

    added docstrings for Task class

--- algo/task/task.py
+++ algo/task/task.py
@@ -1,11 +1,10 @@
-'''
+"""
 Solving task class
-'''
+"""
 
 from enum import Enum
 from typing import (
     List,
-    Dict,
     Callable,
     Optional
 )
@@ -20,11 +19,17 @@ from sklearn.metrics import (
 
 )
 
+"""
+Valid task name
+"""
 class TaskName(Enum): 
     BINARY: str = "binary"
     REG: str = "reg"
 
 
+"""
+Default losses for each task
+"""
 class Metrics(Enum):
     DEFAULT_LOSS_BINARY: Callable = log_loss
     DEFAULT_LOSS_REG: Callable = mean_squared_error
@@ -40,21 +45,54 @@ class Metrics(Enum):
 
 
 class Task:
+    """
+    Description:
+        Solving task class. Defines which task user is to solve. There are only task
+        available (binary classification - `binary` and regression - `reg`)
+
+    Methods:
+        __get_def_task_metric(...) -> Callable:
+            Returns default metric for matched task
+        
+        __get_def_task_loss(...) -> Callable:
+            Returns default loss for matched task
+    """
     def __init__(
         self,
         name: str,
         loss: Optional[str] = None,
         metric: Optional[str] = None,
     ) -> None:
+        """
+        Description:
+            Task class costructor. Initializes task object with name and metric to
+            optimize
+
+        Args:
+            name (str)   : Task name (`binary` - binary classification or `reg` - regression)
+            loss (str)   : Loss to optimize. Optional, if not provided, usning default
+            metric (str) : Metric to compute. Optional, if not provided, usning default
+
+        Returns:
+            None (only initialize Task object)
+        """
         __valid_tasks_names = [tname.value for tname in TaskName] 
         assert name in __valid_tasks_names, \
-        f"Invalid task name {name}, choose one of the following: {__valid_tasks_names}"
+            f"Invalid task name {name}, choose one of the following: {__valid_tasks_names}"
 
         self.name = name
         self.loss = self.__get_def_task_loss() if not loss else loss
         self.metric = self.__get_def_task_metric() if not metric else metric
 
     def __get_def_task_metric(self) -> Callable:
+        """
+        Description:
+            Calls if metric not provided by user
+
+        Returns:
+            Default metric for solving task
+        """
+
         assert self.name, "Provide task name"
 
         return Metrics.DEFAULT_METRIC_BINARY \
@@ -62,6 +100,14 @@ class Task:
             else Metrics.DEFAUTL_METRIC_REG
 
     def __get_def_task_loss(self) -> Callable:
+        """
+        Description:
+            Calls if loss not provided by user
+
+        Returns:
+            Default loss for solving task
+        """
+        
         assert self.name, "Provide task name"
 
         return Metrics.DEFAULT_LOSS_BINARY \
